37ff0435d7cd31f7a8baa06fd854227d844a35bb
Catalin Merfu <catalin.merfu@accendia.com>
2017-04-27 17:55:07 -0400

    Initial Commit

---
+++ choreoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  choreoApp
+//
+//  Created by Mihaela Merfu on 2017-04-27.
+//  Copyright © 2017 Ana Merfu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
